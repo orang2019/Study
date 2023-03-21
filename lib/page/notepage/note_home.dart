eded3804c43f1488eaeac78098160b2d7c9dfb0b
jiwon <jiwonno12345@gmail.com>
2023-03-22 01:22:45 +0900

    bottom bar base

--- lib/page/notepage/note_home.dart
+++ lib/page/notepage/note_home.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:study_app/controller/bottom_tab_bar_controller.dart';
-import 'package:study_app/routes/bottom_tab_bar.dart';
+import 'package:study_app/widget/bottom_tab_bar.dart';
 
 class NoteHome extends StatelessWidget {
   const NoteHome({super.key});
